85a1b63b5122455f22ff3b27ccbfab752fdd403c
zhouhuafei <zhouhuafei@shopex.cn>
2018-05-21 20:00:09 +0800

    h

--- dist/index.js
+++ dist/index.js
@@ -11,32 +11,44 @@ function mkdirs(dirname, callback) {
     }
     fs.stat(dirname, function (error) {
         if (error) {
+            // 文件夹不存在，则创建文件夹
             mkdirs(path.dirname(dirname), function () {
                 fs.mkdir(dirname, callback);
             });
         } else {
-            callback();
+            // 文件夹存在，报错
+            callback(new Error('file already exists'), null);
         }
     });
 }
 
 // 创建多级目录同步
 function mkdirsSync(dirname) {
+    if (fs.existsSync(dirname)) {
+        return true;
+    } else {
+        if (mkdirsSync(path.dirname(dirname))) {
+            fs.mkdirSync(dirname);
+            return true;
+        }
+    }
+}
+
+/*
+function mkdirsSync(dirname) {
     if (!dirname) {
         console.log('路径参数不存在(Path parameters do not exist)');
         return;
     }
-    if (fs.statSync(dirname) !== undefined) {
-        // 返回undefined，表示文件夹存在，否则表示不存在
-        if (mkdirsSync(path.dirname(dirname))) {
-            //
+    if (fs.statSync(dirname) !== undefined) { // 文件夹不存在，则创建文件夹
+        if (mkdirsSync(path.dirname(dirname)) === undefined) {
             fs.mkdirSync(dirname);
-            return true;
         }
-    } else {
-        return true;
+    } else { // 文件夹存在
+        return new Error('file already exists');
     }
 }
+*/
 
 module.exports.mkdirs = mkdirs;
 module.exports.mkdirsSync = mkdirsSync;
--- test/index.test.js
+++ test/index.test.js
@@ -3,30 +3,35 @@ const mkdirs = obj.mkdirs;
 const mkdirsSync = obj.mkdirsSync;
 
 test(`创建多级目录`, () => {
+    /*
     mkdirs('', function (error) {
         if (error) {
             console.log(error);
         } else {
-            console.log('创建成功');
+            console.log('异步创建成功1');
         }
     });
+    */
     mkdirs('./logs/aa1/bb1/cc1', function (error) {
         if (error) {
             console.log(error);
         } else {
-            console.log('创建成功');
+            console.log('异步创建成功2');
         }
     });
-    // const fs = require('fs');
-    // console.log(fs.mkdirSync('./logs/'));
-
     /*
-    const errorInfo = mkdirsSync('./dist/aa2/bb2/cc2');
-    if (errorInfo) {
-        console.log(errorInfo);
+    const errorInfo1 = mkdirsSync('');
+    if (errorInfo1) {
+        console.log(errorInfo1);
     } else {
-        console.log('创建成功');
+        console.log('同步创建成功1');
     }
     */
+    const errorInfo2 = mkdirsSync('./logs/aa2/bb2/cc2');
+    if (errorInfo2) {
+        console.log(errorInfo2);
+    } else {
+        console.log('同步创建成功2');
+    }
     expect(true).toEqual(true);
 });
